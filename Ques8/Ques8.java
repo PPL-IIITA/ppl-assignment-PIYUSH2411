80223175c346763fcf1e3507042348f60ab2fdd8
PIYUSH2411 <piyushgupta2411@gmail.com>
2017-04-10 12:20:15 +0530

    Update Ques8.java

--- Ques8/Ques8.java
+++ Ques8/Ques8.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ques8;
+//package ques8;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
