e5d828f0888db1b0bde7f79a831d9bf5ef357a4a
PIYUSH2411 <piyushgupta2411@gmail.com>
2017-04-10 11:59:32 +0530

    Create Ques2.java

--- Ques2/Ques2.java
+++ Ques2/Ques2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ques2;
+//package ques2;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
